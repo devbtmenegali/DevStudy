b85055fca9ca09c3e6144fb3471a7db8d1a692f6
devbtmenegali <brunotmenegali@gmail.com>
2024-06-28 16:30:01 -0300

    first commit

---
+++ Atividade 1/BC_1005_Average1.py
@@ -0,0 +1,7 @@
+# ATIVIDADE 1005 Average1
+
+A = float(input())
+B = float(input())
+average = (A * 3.5 + B * 7.5) / 11
+
+print(f"MEDIA = {average:.5f}")
---
+++ Atividade 1/BC_1007_Difference.py
@@ -0,0 +1,9 @@
+# ATIVIDADE 1007 Difference
+
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+Difer = (A*B) - (C*D)
+
+print(f"DIFERENCA = {Difer}")
