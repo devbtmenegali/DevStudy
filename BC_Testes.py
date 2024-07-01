e7f43a8b2d3c0d8ce0097e19babb1786d1598510
devbtmenegali <brunotmenegali@gmail.com>
2024-06-30 23:32:15 -0300

    first commit

--- BC_Testes.py
+++ BC_Testes.py
@@ -1,20 +1,11 @@
+product_prices = {1: 4.00, 2: 4.50, 3: 5.00, 4: 2.00, 5: 1.50}
 
-valor = float(input())                                     
+code, quantity = map(int, input().split())
 
-valor_centavos = int(valor * 100)                           # Converte para centavos (integer)
-
-notas = [10000, 5000, 2000, 1000, 500, 200]                 # em centavos
-moedas = [100, 50, 25, 10, 5, 1]
-
-print("NOTAS:")
-
-for nota in notas:
-    quantidade = valor_centavos // nota
-    print(f"{quantidade} nota(s) de R$ {nota/100:.2f}")     # Converter para reais
-    valor_centavos %= nota
-
-print("MOEDAS:")
-for moeda in moedas:
-    quantidade = valor_centavos // moeda
-    print(f"{quantidade} moeda(s) de R$ {moeda/100:.2f}")   # Converter para reais
-    valor_centavos %= moeda
+if code in product_prices:
+    price = product_prices[code]
+    total = quantity * price
+    print(f"Total: R$ {total:.2f}")
+else:
+    print("Código Inválido")
+    
--- Tarefa 2_28.06/BC_1038_Snack.py
+++ Tarefa 2_28.06/BC_1038_Snack.py
@@ -1,5 +1,12 @@
+product_prices = {1: 4.00, 2: 4.50, 3: 5.00, 4: 2.00, 5: 1.50}
 
-X,Y = map(int,input().split())
-Total = X + Y
+code, quantity = map(int, input().split())
 
-print(f"{Total}")
+if code in product_prices:
+    price = product_prices[code]            # Pega o preço do produto
+    total = price * quantity                # Calcula o custo
+    
+    print(f"Total: R$ {total:.2f}")         # Imprimir preço total
+
+else:
+    print("Código inválido")                # Mensagem de Erro
