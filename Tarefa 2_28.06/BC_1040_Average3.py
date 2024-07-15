46c540314075d39d255b019db334c09cad641d0e
devbtmenegali <brunotmenegali@gmail.com>
2024-07-15 18:48:27 -0300

    third commit

--- Tarefa 2_28.06/BC_1040_Average3.py
+++ Tarefa 2_28.06/BC_1040_Average3.py
@@ -2,23 +2,22 @@ N1, N2, N3, N4 = map(float, input().split())
 
 media = (N1 * 2 + N2 * 3 + N3 * 4 + N4 * 1) / 10
 
-print(f"Media: {media:.1f}")
-
 if media >= 7.0:
     print("Aluno aprovado.")  
 elif media < 5.0:
     print("Aluno reprovado.") 
 else:  
-    print("Aluno em exame.")  
-
     exame = float(input())
-
-    print(f"Nota do exame: {exame:.1f}")
-
     media_final = (media + exame) / 2
-
-    print(f"Media final: {media_final:.1f}")
+    print(f"Media: {media:.1f}")
+    print(f"Aluno em exame.")
+    print(f"Nota do exame: {exame:.1f}")
+    
     if media_final >= 5.0:
-        print("Aluno aprovado.")  
+        print("Aluno aprovado.")
+        print(f"Media final: {media_final:.1f}")
+  
     else:
-        print("Aluno reprovado.") 
+        print("Aluno reprovado.")
+        print(f"Media final: {media_final:.1f}")
+ 
