3cc795f77814c2f465eea6392925cbbe0c578765
devbtmenegali <brunotmenegali@gmail.com>
2024-07-18 16:19:54 -0300

    lista refeicao v2

--- Tarefa 5_16.07/T23_refeicao.py
+++ Tarefa 5_16.07/T23_refeicao.py
@@ -2,8 +2,6 @@
 #café da manhã, almoço e jantar. Crie um programa em Python que 
 #combine essas listas em uma única lista usando o método extend()
 
-comida_do_dia = []
-
 def cafe_da_manha ():
     
     lista_manha = []
@@ -12,5 +10,32 @@ def cafe_da_manha ():
         lista_manha.append(cafe)
     
     print(f"Café da Manhã: {lista_manha}")
+    return lista_manha
+
+def comida_almoco ():
+    
+    lista_almoco = []
+    for a in range(1,4):
+        almoco = input(f" Itens {a} do Almoço: ")
+        lista_almoco.append(almoco)
+    
+    print(f"Lista do Almoco: {lista_almoco}")
+    return lista_almoco
+
+def comida_jantar ():
+    
+    lista_jantar = []
+    for a in range(1,4):
+        jantar = input(f" Itens {a} da janta: ")
+        lista_jantar.append(jantar)
+    
+    print(f"Lista do jantar: {lista_jantar}")
+    return comida_jantar
 
 cafe_da_manha()
+comida_almoco()
+comida_jantar()
+
+lista_manha.extend(lista_almoco)
+lista_manha.extend(lista_jantar)
+print(lista_manha)
