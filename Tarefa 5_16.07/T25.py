a1d9847aa1de8d549e6064d2fa79e8af61e2c96c
devbtmenegali <brunotmenegali@gmail.com>
2024-07-18 18:40:13 -0300

    lista festa v2

--- Tarefa 5_16.07/T25.py
+++ Tarefa 5_16.07/T25.py
@@ -2,8 +2,9 @@
 #Ela quer adicionar um novo convidado na quarta posição da lista. 
 #Crie um programa em Python que receba os nomes dos convidados e insira o novo convidado na posição correta.
 
+lista_da_festa = []
+
 def convidados_festa():
-    lista_da_festa = []
 
     for i in range (1,7):
         convidado = input("Digite o nome do convidado:").strip()
@@ -23,8 +24,13 @@ def convidados_festa():
 
     return lista_da_festa
 
+def incluir_convidado():
 
+    nome_novo_convidado = input("Digite o nome para incluir:")
+    lista_da_festa.insert(4, nome_novo_convidado)
+
+    print(lista_da_festa)
 
 convidados_festa()
 
-#def incluir_convidado():
+incluir_convidado()
