95e4dc7d258f7900e9b3630f29ad927c323d4e27
devbtmenegali <brunotmenegali@gmail.com>
2024-07-19 17:22:50 -0300

    LojaOnlineV3

--- Tarefa 5_16.07/T24_LojaOnLine.py
+++ Tarefa 5_16.07/T24_LojaOnLine.py
@@ -2,14 +2,14 @@
 #A loja possui várias categorias de produtos e cada categoria tem sua própria lista de produtos. 
 #Crie um programa em Python que combine todas as listas de produtos em uma única lista e exiba a lista 
 #final de produtos disponíveis na loja.
-
 lista_produtos = []
 lista_tenis = []
 lista_roupas = []
 lista_cama = []
 
 def categoria_tenis ():
-    lista_tenis = []
+    global lista_tenis
+
     while True:
         marcas_tenis = input("Adicione uma marca de tenis:")
 
@@ -20,52 +20,55 @@ def categoria_tenis ():
             categoria_roupa()
 
         lista_tenis.append(marcas_tenis)
-    
+
     print(f"A lista de tenis contém: {lista_tenis}")
-    return lista_tenis
+    categoria_roupa() 
 
 def categoria_roupa ():
-    lista_roupas = []
+    global lista_roupas
+
     while True:
         marcas_roupas = input("Adicione uma marca de roupas:")
 
         if marcas_roupas == "finalizar":
+            print(f"A lista de roupa contém: {lista_roupas}")
             break
+
         elif marcas_roupas == "cama":
             categoria_cama()
 
         lista_roupas.append(marcas_roupas)
     
     print(f"A lista de roupa contém: {lista_roupas}")
-    return lista_roupas
+    categoria_cama() 
 
 def categoria_cama ():
-    lista_cama = []
+    global lista_cama
+
     while True:
         marcas_cama = input("Adicione uma marca de roupa de cama:")
 
         if marcas_cama == "finalizar":
+            print(f"A lista de roupa de cama contém: {lista_cama}")
             break
         elif marcas_cama == "listar":
-            print(lista_produtos)
+            listar_todos_produtos()
 
         lista_cama.append(marcas_cama)
-    
+
     print(f"A lista de roupa de cama contém: {lista_cama}")
-    return lista_cama
 
 
-lista_tenis = categoria_tenis()
-lista_roupas = categoria_roupa()
-lista_cama = categoria_cama()
+def listar_todos_produtos ():
 
-lista_produtos.extend(lista_tenis)
-lista_produtos.extend(lista_roupas)
-lista_produtos.extend(lista_cama)    
-print(lista_produtos)
+    lista_produtos.extend(lista_tenis)
+    lista_produtos.extend(lista_roupas)
+    lista_produtos.extend(lista_cama)    
+    print("A lista de produtos contém:", lista_produtos)
+
+listar_todos_produtos()
 
 categoria_tenis()
-categoria_roupa()
-categoria_cama()
+
 
 
