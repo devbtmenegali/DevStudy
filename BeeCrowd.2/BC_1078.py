73a2af840df7752aa48548232af10f9d1f94a3b5
devbtmenegali <brunotmenegali@gmail.com>
2024-07-21 12:42:46 -0300

    BC_1078_Accepted

---
+++ BeeCrowd.2/BC_1078.py
@@ -0,0 +1,13 @@
+
+def multiplication():
+
+    N = int(input())
+    N > 1 and N < 1000
+
+    for n in range (10):
+        n += 1
+        result = n * N
+
+        print(f" {n} x {N} = {result}")
+
+multiplication()
