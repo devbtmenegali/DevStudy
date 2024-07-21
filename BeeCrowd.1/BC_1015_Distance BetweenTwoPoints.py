6c53e262ea1a8bf3c0c4ebd42504a3efcda9cfe6
devbtmenegali <brunotmenegali@gmail.com>
2024-07-21 15:14:35 -0300

    BC_1015_Accepted

--- BeeCrowd.1/BC_1015_Distance BetweenTwoPoints.py
+++ BeeCrowd.1/BC_1015_Distance BetweenTwoPoints.py
@@ -1,10 +1,9 @@
 #ATIVIDADE 1015 Distance Between Points
 
-import math                                     #permite que você utilize as funções e constantes presentes no módulo math (funções matemáticas).
-
+import math                                  
 x1, y1 = map(float, input().split())
 x2, y2 = map(float, input().split())
 
-distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)   #sqrt é raiz quadrada.
+distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)  
 
 print(f"{distance:.4f}")
