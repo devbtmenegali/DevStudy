1dcd1aa55e2d31b1771c031ccc9dbac0f864d670
devbtmenegali <brunotmenegali@gmail.com>
2024-07-22 14:54:31 -0300

    BC_1064_Accepted

--- BeeCrowd.2/BC_1064_PositiveAndAverage.py
+++ BeeCrowd.2/BC_1064_PositiveAndAverage.py
@@ -1,18 +1,20 @@
 
 def positives_and_average():
    
-    positive_numbers = 0  
-    numeros = []
+    positive_numbers = []  
+    n_positive_numbers = 0
 
     for n in range (6):
         n = float(input( ))
-        numeros.append(n)
-        average = sum(numeros)/len(numeros)
+        #numeros.append(n)
+        
 
         if n > 0:
-            positive_numbers += 1  
-
-    print(f"{positive_numbers} valores positivos")
+            positive_numbers.append(n)  
+            n_positive_numbers += 1
+            average = sum(positive_numbers) / len(positive_numbers)
+    
+    print(f"{n_positive_numbers} valores positivos")
     print(f"{average:.1f}")  
 
 positives_and_average()
