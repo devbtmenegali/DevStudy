d42cf8acc946e6f154e2ede4c639d14b39e286c6
devbtmenegali <brunotmenegali@gmail.com>
2024-07-22 17:23:44 -0300

    BC_1061_NoAccepted

---
+++ BeeCrowd.2/BC_1061.py
@@ -0,0 +1,22 @@
+
+from datetime import datetime
+
+dia_inicio = int(input("Dia "))  
+hora_inicio, minuto_inicio, segundo_inicio = map(int, input().split(":"))
+
+dia_fim = int(input("Dia "))
+hora_fim, minuto_fim, segundo_fim = map(int, input().split(":"))
+
+inicio = datetime(datetime.now().year, 4, dia_inicio, hora_inicio, minuto_inicio, segundo_inicio)
+fim = datetime(datetime.now().year, 4, dia_fim, hora_fim, minuto_fim, segundo_fim)
+
+duration = fim - inicio
+
+dias = duration.days
+horas, remainder = divmod(duration.seconds, 3600)  
+minutos, segundos = divmod(remainder, 60)       
+
+print(f"{dias} dia(s)")
+print(f"{horas} hora(s)")
+print(f"{minutos} minuto(s)")
+print(f"{segundos} segundo(s)")
